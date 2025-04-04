85a1246a7faf748971ad41af042af81df6b40187
Giorgi Giguashvili <giguashviligiorgi50@gmail.com>
2025-04-05 00:06:41 +0400

    fix DEBUG issue.

--- core/wsgi.py
+++ core/wsgi.py
@@ -3,7 +3,7 @@ from django.core.wsgi import get_wsgi_application
 from core.settings.base import DEBUG  # Import DEBUG from the base settings
 
 # Use DEBUG to set the appropriate settings module for WSGI
-if DEBUG:
+if DEBUG == "True":
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.development')
 else:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.production')
