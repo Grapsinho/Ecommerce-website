11c51faafa467983fda1b7956a3146990a3b95c8
Giorgi Giguashvili <giguashviligiorgi50@gmail.com>
2025-04-05 19:45:55 +0400

    still fixing cloudinary issues.

--- core/settings/production.py
+++ core/settings/production.py
@@ -50,8 +50,14 @@ cloudinary.config(
     cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
     api_key=os.environ.get("CLOUDINARY_API_KEY"),
     api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
-    prefix = "avatars/"
 )
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    'API_KEY': os.environ.get("CLOUDINARY_API_KEY"),
+    'API_SECRET': os.environ.get("CLOUDINARY_API_SECRET"),
+    'PREFIX': 'avatars'  # 👈 this is where you define the folder Cloudinary should use
+}
 
 INSTALLED_APPS += [
     "cloudinary",
