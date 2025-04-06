e103fd7e95c29a6a3fae0f8210be63482f301faa
Giorgi Giguashvili <giguashviligiorgi50@gmail.com>
2025-04-06 14:06:13 +0400

    it is a good practice to have both authentication class and permission class so i added permission class in protected view.

--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.exceptions import TokenError
@@ -284,6 +284,8 @@ class UserProfileView(APIView):
     """
 
     authentication_classes = [JWTAuthentication]
+
+    permission_classes = [IsAuthenticated]
 
     @extend_schema(
         summary="Get User Profile by ID",
